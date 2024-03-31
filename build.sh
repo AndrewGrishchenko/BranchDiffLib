3d464a5d573c27b5b425ef8fbe40c527af0b9ba4
AndrewGrishchenko <andrew26080537@gmail.com>
2024-03-31 06:36:57 +0300

    added cmake

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-gcc -c -fpic -o branchdiff.o branchdiff.c
-gcc -shared -o libbranchdiff.so branchdiff.o
--- src/branchdiff.h
+++ src/branchdiff.h
@@ -1,5 +1,5 @@
-#ifndef mars_h__
-#define mars_h__
+#ifndef branchdiff_h__
+#define branchdiff_h__
 
 struct MessageStruct;
 extern void presenceDiff (MessageStruct* packages1, int packages1Num, MessageStruct* packages2, int packages2Num, MessageStruct** responseVals, int* responseNumVals);
